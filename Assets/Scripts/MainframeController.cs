2a10cf110e5adc30177af3903be18a0e4d87ecd3
Rafael Hocevar <hocevar.rafael@gmail.com>
2018-01-27 11:15:08 -0800

    New file prefab

---
+++ Assets/Scripts/MainframeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class MainframeController : MonoBehaviour {
+
+    private LayerMask draggableLayer;
+
+    private void Awake()
+    {
+        draggableLayer = LayerMask.NameToLayer("Draggable");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.IsInLayer(draggableLayer))
+        {
+            Debug.Log("Draggable inside");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.IsInLayer(draggableLayer))
+        {
+            Debug.Log("Draggable outside");
+        }
+    }
+
+}
