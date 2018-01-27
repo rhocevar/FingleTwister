5772cb2e4a67af5b426ef4bf0d4741f45fc30fcf
Rafael Hocevar <hocevar.rafael@gmail.com>
2018-01-26 21:38:05 -0800

    .

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,117 @@
+﻿/***************************************************************************************************
+ *  Script name :   GameManager.cs
+ *  Created on  :   09.01.2016
+ *  Author      :   Rafael Hocevar
+ *  Purpose     :   Controls the game flow, win condition, scene transitions, framerate and 
+ *                  keeps track of the number of chapters and levels.
+*****************************************************************************************************/
+
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Utils;
+
+/// <summary>
+/// Controls the game flow, win condition, scene transitions, framerate and keeps track of the number of chapters and levels.
+/// </summary>
+public class GameManager : Singleton<GameManager>
+{
+    #region Inspector
+    /// The total number of chapter in the game.
+    [SerializeField] private const int nChapters = 1;
+    /// The total number o level for each chapter.
+    [SerializeField] private const int nLevelsPerChapter = 3;
+    #endregion
+
+    #region Attributes
+    private FPSDisplay fpsDisplay;
+    private Dictionary<int, Level[]> levels;
+	/// The framerate will be fixed at the specified target fps.
+    [SerializeField] private int targetFramerate = 60;
+    [SerializeField] private int captureFramerate = 40;
+    #endregion
+
+    #region Properties
+    /// Reference to the display the FPS counter on the screen. Currently used by the InputManager.
+    public FPSDisplay FPSDisplay { get { return fpsDisplay; } }
+    /// Dictionary of levels: Chapter -> Levels array
+    public Dictionary<int, Level[]> Levels { get { return levels; } set { levels = value; } }
+    #endregion
+
+    #region Private Methods
+    protected override void Awake()
+    {
+        base.Awake();
+        #if UNITY_IOS
+            Application.targetFrameRate = 30;
+        #else
+            Application.targetFrameRate = targetFramerate;
+            Time.captureFramerate = captureFramerate; //The higher the slower
+        #endif
+        fpsDisplay = GetComponent<FPSDisplay>();
+    }
+
+    private void Start()
+    {
+        InitializeLevelsDictionary();
+        SceneManager.sceneLoaded += OnSceneLoaded; //Subscribe for the sceneLoaded event
+    }
+    /// Initializes the dictionary structure to hold the levels and chapters.
+    private void InitializeLevelsDictionary()
+    {
+        levels = new Dictionary<int, Level[]>();
+
+        for (int ch = 1; ch <= nChapters; ch++)
+        {
+            Level[] lvls = new Level[nLevelsPerChapter];
+            for (int lev = 0; lev < nLevelsPerChapter; lev++)
+            {
+                lvls[lev] = new Level(lev + 1, ch);
+            }
+            levels.Add(ch, lvls);
+        }
+        //Unlock first level
+        levels[1][0].IsLocked = false;
+    }
+
+	private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == (int)Scenes.Menu)
+        {
+            UIManager.Instance.ShowInGameUI(false);
+            UIManager.Instance.ShowMainMenu(true);
+        }
+        else
+        {
+            UIManager.Instance.ShowInGameUI(true);
+            UIManager.Instance.ShowMainMenu(false);
+        }
+    }
+	#endregion
+
+	#region Public Methods
+    /// <summary>
+    /// Loads a scene based on the scene name passed as the parameter.
+    /// </summary>
+    /// <param name="s"> The name of the scene. </param>
+    public void LoadScene(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+	/// <summary>
+	/// Loads a scene based on the scene name passed as the parameter.
+	/// </summary>
+	/// <param name="i"> The name of the scene. </param>
+	public void LoadScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+    /// <summary>
+    /// Exits the game.
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// When a class derives from this they will be a singleton don't derive from the derived class 
+/// </summary>
+/// <typeparam name="T">The generic value have to be the derived class</typeparam>
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+	//Exposed to the inspector whether or not it should be destroyed on a scene load
+	[SerializeField]
+	private bool dontDestroyOnload = false;
+
+	//Instance for singleton
+	private static T instance;
+
+	//Getting the instance for other classes
+	public static T Instance { get { return instance; } }
+
+	/// <summary>
+	/// Unity awake method can be overriden but remember to base it
+	/// </summary>
+	protected virtual void Awake()
+	{
+		if (instance)
+		{
+			//Destroying this if there already exists an instance
+			Destroy(gameObject);
+		}
+		else
+		{
+			//Setting the instance
+			instance = (T)this;
+
+			if (dontDestroyOnload)
+			{
+				//Don't destroy on load if enabled
+				DontDestroyOnLoad(this);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Unity OnDestroy method can be overriden but remember to base it
+	/// </summary>
+	protected virtual void OnDestroy()
+	{
+		if (instance == this)
+		{
+			//Sets instance to null when the instance is destroyed 
+			instance = null;
+		}
+	}
+}
