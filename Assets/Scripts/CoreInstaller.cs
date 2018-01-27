e6ab43461b71546a97e8631e91cbf351da241d83
Hector Alzate <halzate93@gmail.com>
2018-01-27 10:00:57 -0800

    Dragging thingd

---
+++ Assets/Scripts/CoreInstaller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class CoreInstaller : MonoInstaller 
+{
+
+	public override void InstallBindings()
+	{
+		Container.Bind<GameManager> ().FromComponentInHierarchy ().AsSingle ();
+	}
+}
--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -2,25 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Zenject;
 
 [RequireComponent (typeof (Rigidbody2D))]
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public void OnBeginDrag(PointerEventData eventData)
+	[Inject]
+	private GameManager manager;
+	private new Rigidbody2D rigidbody;
+
+	private void Awake ()
+	{
+		rigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+    public void OnBeginDrag (PointerEventData eventData)
     {
 
     }
 
-    public void OnDrag(PointerEventData eventData)
+    public void OnDrag (PointerEventData eventData)
     {
-		if (eventData.dragging)
+		if (eventData.pointerCurrentRaycast.gameObject == gameObject)
 		{
 			Debug.Log (eventData.pointerCurrentRaycast.worldPosition);
-			transform.position = eventData.pointerCurrentRaycast.worldPosition;
+			rigidbody.position = eventData.pointerCurrentRaycast.worldPosition;
 		}
     }
 
-    public void OnEndDrag(PointerEventData eventData)
+    public void OnEndDrag (PointerEventData eventData)
     {
 
     }
