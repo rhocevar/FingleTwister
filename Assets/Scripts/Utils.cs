384ac2756ce6d1e1f4349f6a4f139093bac347a6
Hector Alzate <halzate93@gmail.com>
2018-01-27 10:43:01 -0800

    Utils layer checking

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils 
+{
+	public static bool IsInLayer (this GameObject toCheck, LayerMask mask)
+	{
+		return ((mask.value & (1 << toCheck.layer)) > 0);
+	}
+}
