d5e89735ce438d3a00d9873f477fa454a817fd11
Hector Alzate <halzate93@gmail.com>
2018-01-27 10:25:02 -0800

    Prefab for Draggable and namespace Dragging

---
+++ Assets/Scripts/Dragging/Draggable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
+
+namespace Dragging
+{
+	[RequireComponent (typeof (Rigidbody2D))]
+	public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+	{
+		[Inject]
+
+		private new Rigidbody2D rigidbody;
+
+		private void Awake ()
+		{
+			rigidbody = GetComponent<Rigidbody2D> ();
+		}
+
+		public void OnBeginDrag (PointerEventData eventData)
+		{
+
+		}
+
+		public void OnDrag (PointerEventData eventData)
+		{
+			if (eventData.pointerCurrentRaycast.gameObject == gameObject)
+			{
+				//Debug.Log (eventData.pointerCurrentRaycast.worldPosition);
+				rigidbody.position = eventData.pointerCurrentRaycast.worldPosition;
+			}
+		}
+
+		public void OnEndDrag (PointerEventData eventData)
+		{
+
+		}
+	}
+}
