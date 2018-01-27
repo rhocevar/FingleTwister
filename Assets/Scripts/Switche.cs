3730846725d36e17e1d7d797dfad7a97eb9e00f6
Hector Alzate <halzate93@gmail.com>
2018-01-27 14:57:04 -0800

    Working on switch

---
+++ Assets/Scripts/Switche.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Switche : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
