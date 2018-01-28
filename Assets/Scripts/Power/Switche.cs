fa514b9088e6f528694b23bb2718f6d403a02a6c
Hector Alzate <halzate93@gmail.com>
2018-01-27 16:06:11 -0800

    Switches

---
+++ Assets/Scripts/Power/Switche.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
+
+namespace Power
+{
+    [RequireComponent (typeof (Collider2D))]
+    public class Switche : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IPointerEnterHandler
+    {
+        [Inject]
+        private PowerController powerController;
+        private bool isOn;
+        
+        private void Start ()
+        {
+            powerController.AddSwitche ();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            TryTurnOn ();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            TryTurnOff ();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            TryTurnOff ();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            TryTurnOn ();
+        }
+
+        private void TryTurnOn ()
+        {
+            if (isOn) return;
+            isOn = true;
+            GetComponent<Renderer> ().material.color = Color.green;
+            powerController.TurnOnSwitche ();
+        }
+
+        private void TryTurnOff ()
+        {
+            if (!isOn) return;
+            isOn = false;
+            GetComponent<Renderer> ().material.color = Color.white;
+            powerController.TurnOffSwitche ();
+        }
+    }
+}
