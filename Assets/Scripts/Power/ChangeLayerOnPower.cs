66d6bb220928f75e81dc400a0da1d72644286e8b
Hector Alzate <halzate93@gmail.com>
2018-01-28 14:20:35 -0800

    Tweaked animation on screen

---
+++ Assets/Scripts/Power/ChangeLayerOnPower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Power
+{
+	public class ChangeLayerOnPower : BaseElectricObject 
+	{
+		[SerializeField]
+		private SpriteRenderer sprite;
+		[SerializeField]
+		private string onLayer;
+		[SerializeField]
+		private string offLayer;
+
+		protected override void OnPowerChanged(bool isEnabled) 
+		{
+			base.OnPowerChanged (isEnabled);
+			sprite.sortingLayerName = isEnabled? onLayer : offLayer;	
+		}
+	}
+
+}
